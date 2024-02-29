fe4c64ee0c669f6cebb6be8703674ada3ed4665c
DinaDi025 <di.serdyuck0201@gmail.com>
2024-02-29 17:59:49 +0200

    Initial commit

---
+++ src/modules/repository/services/user.repository.ts
@@ -0,0 +1,11 @@
+import { DataSource, Repository } from 'typeorm';
+
+import { UserEntity } from '../../../database/entities/user.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository extends Repository<UserEntity> {
+  constructor(private readonly dataSource: DataSource) {
+    super(UserEntity, dataSource.manager);
+  }
+}
